176f3a6eff0cdb6d0e7ebf5efc59c00146694d54
Yury Martynov <email@linxon.ru>
2020-01-17 17:58:16 +0300

    misc: update qkernel-params.sh

--- misc/qkernel-params/qkernel-params.sh
+++ misc/qkernel-params/qkernel-params.sh
@@ -1,14 +1,14 @@
 #!/bin/bash +x
 
-export \
-	__HOME_DIR__="${QKERNEL_HOME_DIR:-/tmp/$(basename $0)}"
-	__CEC_STATE_FILE__="${__HOME_DIR__}/.check_extra_config_state" \
-	PORTAGE_VDB_PATH="${PORTAGE_VDB_PATH:-/var/db/pkg}" \
-	PORTAGE_BIN_PATH="${PORTAGE_BIN_PATH:-/usr/lib/portage/python$(python -c '\
-		import sys;\
-		version=sys.version_info[:2];\
-		print("{0}.{1}".format(*version))'
-	)}"
+PORTAGE_VDB_PATH="${PORTAGE_VDB_PATH:-/var/db/pkg}"
+PORTAGE_BIN_PATH="${PORTAGE_BIN_PATH:-/usr/lib/portage/python$(python -c '\
+	import sys;\
+	version=sys.version_info[:2];\
+	print("{0}.{1}".format(*version))'
+)}"
+
+export __HOME_DIR__="${QKERNEL_HOME_DIR:-/tmp/$(basename $0)}"
+export __CEC_STATE_FILE__="${__HOME_DIR__}/.check_extra_config_state"
 
 source "${PORTAGE_BIN_PATH}/isolated-functions.sh" || exit 1
 source "${PORTAGE_BIN_PATH}/phase-functions.sh" || die
@@ -59,7 +59,7 @@ do
 			update_moduledb multibuild_copy_sources multibuild_merge_root
 			update_depmod eapply eapply_user enewuser enewgroup gnome2_environment_reset
 			python_setup python-any-r1_pkg_setup debug-print debug-print-function
-			debug-print-section tc-ld-disable-gold tc-export linux-mod_pkg_setup'
+			debug-print-section tc-ld-disable-gold tc-export linux-mod_pkg_setup multilib_layout'
 
 		__is_func__ 'pkg_pretend' && pkg_pretend
 		__is_func__ 'pkg_setup' && pkg_setup
